d23b21fdd5e12e05401caf31e4fd50a8d24430d8
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-08-31 08:44:42 +0200

    * First commit

---
+++ test/master.js
@@ -0,0 +1,14 @@
+let nonCallable = 1;
+let other = 2;
+
+function withTooManyParams(a, b, c, d, e) {
+    return a + b + c + d + e;
+}
+
+;;;;
+;;;;;;;;;;;;;;
+
+nonCallable();
+
+var myWindow = document.getElementById('myIFrame').contentWindow;
+myWindow.postMessage(message, "*");
