9ef9ffbff5b63028a0bc48099c09a975cf84e655
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-08-31 09:06:46 +0200

    * Second commit

---
+++ test/tooLarge.js
@@ -0,0 +1,18 @@
+let nonCallable = 1;
+let other = 2;
+
+let a1 = 2;
+let a2 = 2;
+let a3 = 2;
+let a4 = 2;
+let a5 = 2;
+let a6 = 2;
+let a7 = 2;
+let a8 = 2;
+let a9 = 2;
+let a10 = 2;
+let a11 = 2;
+let a12 = 2;
+
+
+nonCallable();
